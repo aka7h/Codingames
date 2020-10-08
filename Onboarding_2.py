eb5e3724d60b7af3a79bff917892e613d22e2b0d
Akash (Sathya) Kothapalli <aka7h.sathya@hotmail.com>
2020-10-08 09:58:47 +0530

    Onboarding update

---
+++ Onboarding_2.py
@@ -0,0 +1,11 @@
+import sys
+import math
+
+# game loop
+while True:
+    enemy_1 = input()  # name of enemy 1
+    dist_1 = int(input())  # distance to enemy 1
+    enemy_2 = input()  # name of enemy 2
+    dist_2 = int(input())  # distance to enemy 2
+
+    print(enemy_2 if dist_2 < dist_1 else enemy_1)
